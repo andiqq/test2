5e38f2c5eccd653df041094a00eb4cafb5f6c484
Dr. Andreas Plagens <andreas@plagens.eu>
2022-03-19 08:35:11 +0100

    Initial Commit

---
+++ test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Dr. Andreas Plagens on 19.03.22.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
